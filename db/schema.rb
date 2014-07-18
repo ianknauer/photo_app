0ee0b4d4c895285bc4cc14890a3e14ab8481913c
ianknauer <ian.knauer@gmail.com>
2014-07-17 21:38:17 -0700

    create album migration + model spec test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717164931) do
+ActiveRecord::Schema.define(version: 20140718043429) do
+
+  create_table "albums", force: true do |t|
+    t.string "name"
+  end
 
   create_table "customers", force: true do |t|
     t.text     "name"
