2444818edcc353e9238428b48e70bd7d1816f455
ianknauer <ian.knauer@gmail.com>
2014-09-09 18:00:10 -0700

    testing without user

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -4,7 +4,7 @@ class AlbumsController < ApplicationController
   end
 
   def new
-    @customer = Customer.find_by(slug: params[:id])
+    #@customer = Customer.find_by(slug: params[:id])
     @album = Album.new
     @album.pictures.build
   end
