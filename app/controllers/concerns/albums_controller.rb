d97de5d9041c398676ee614a50a38ff4a57d4cab
ianknauer <ian.knauer@gmail.com>
2014-09-09 18:43:58 -0700

    adding to concerns

--- app/controllers/concerns/albums_controller.rb
+++ app/controllers/concerns/albums_controller.rb
@@ -1,23 +1,36 @@
-class AlbumsController < ApplicationController 
+class AlbumsController < ApplicationController
+
+  def index
+  end
 
   def new
+    @customer = Customer.find_by(slug: params[:id])
     @album = Album.new
+    @album.pictures.build
   end
 
   def create
-    @album = Album.new(user_params)
+    @album = Album.new(merge_album_and_customer)
     if @album.save
-      flash[:notice] = "album is created!"
-      redirect_to root_path
+      params[:pictures]['small_thumb'].each do |a|
+        @picture = @album.pictures.create!(:small_thumb => a, :album_id => @album.id, :name => "image")
+      end
+      redirect_to customers_path
     else
       render :new
     end
+  end
+  def show
+    @album = Album.find_by slug: params[:id]
   end
 
   private
 
   def album_params
-    params.require(:album).permit(:name)
+    params.require(:album).permit(:customer_id, :name, pictures_attributes:[:small_thumb])
   end
 
+  def merge_album_and_customer
+    album_params.merge!(customer_id: (Customer.find_by(slug: params[:id]).id))
+  end
 end
