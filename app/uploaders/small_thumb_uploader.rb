f7f12270cb2f26b008bceb4d7bbf91042d96d3a6
ianknauer <ian.knauer@gmail.com>
2014-09-10 21:45:14 -0700

    fix an error

--- app/uploaders/small_thumb_uploader.rb
+++ app/uploaders/small_thumb_uploader.rb
@@ -1,7 +1,7 @@
 class SmallThumbUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process :resize_to_fill => [236,166]
-  if Rails.en.production?
+  if Rails.env.production?
     storage :fog
   end
 end
