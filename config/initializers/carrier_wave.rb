bc212edab1131b89a271f77807ef054db21a0c4a
ianknauer <ian.knauer@gmail.com>
2014-09-11 10:34:07 -0700

    different credentials

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,12 +3,12 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :aws_access_key_id      => 'AKIAINVANZB2ALQRR2F',                # required
-      :aws_secret_access_key  => 'qYJkoJimYJwHJ6B1mHMEYRA9KP9X7a0djYxUY5Ev',             # required
+      :aws_access_key_id      => 'AKIAITTM5YY2CCO6ZLBA',                # required
+      :aws_secret_access_key  => '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',             # required
     }
     config.fog_directory  = 'iank-photo-app'                # required
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
   end
-end
+ends
